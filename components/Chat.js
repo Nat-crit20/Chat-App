c34074f53633c0e502f6234cf94e0f21a6bfd7d7
Nat-crit20 <greuelnathan74@gmail.com>
2023-05-25 09:09:47 -0500

    Added GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,52 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, chatColor } = route.params;
+  const [messages, setMessage] = useState([]);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessage([
+      {
+        _id: 1,
+        text: "Hello Developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "Welcome to the chat",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
+
+  const onSend = (newMessage) => {
+    setMessage((prevMessage) => GiftedChat.append(prevMessage, newMessage));
+  };
+
   return (
     <View style={[styles.container, { backgroundColor: chatColor }]}>
-      <Text></Text>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+      {Platform.OS === "ios" ? (
+        <KeyboardAvoidingView behavior="padding" />
+      ) : null}
     </View>
   );
 };
@@ -17,8 +54,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
