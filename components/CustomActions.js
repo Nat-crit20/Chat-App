faf533296096fc613c31bb98ab01921bf424e984
Nat-crit20 <greuelnathan74@gmail.com>
2023-06-15 08:41:00 -0500

    refactored the code

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -62,23 +62,26 @@ const CustomActions = ({
     return `${userID}-${timeStamp}-${imageName}`;
   };
 
+  const uploadAndSendImage = async (imageURI) => {
+    const uniqueRefString = generateReference(imageURI);
+    const response = await fetch(imageURI);
+    const blob = await response.blob();
+    const newUploadRef = ref(storage, uniqueRefString);
+
+    uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+      console.log("File has been uploaded successfully");
+      const imageURI = await getDownloadURL(snapshot.ref);
+      onSend({ image: imageURI });
+    });
+  };
+
   const pickImage = async () => {
     let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     if (permissions?.granted) {
       let result = await ImagePicker.launchImageLibraryAsync();
       if (!result.canceled) {
-        const imageURI = result.assets[0].uri;
-        const uniqueRefString = generateReference(imageURI);
-        const response = await fetch(imageURI);
-        const blob = await response.blob();
-        const newUploadRef = ref(storage, uniqueRefString);
-
-        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
-          console.log("File has been uploaded successfully");
-          const imageURI = await getDownloadURL(snapshot.ref);
-          onSend({ image: imageURI });
-        });
+        await uploadAndSendImage(result.assets[0].uri);
       } else Alert.alert("Permissions haven't been granted");
     }
   };
