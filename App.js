280cd5df0e9e3a6982d95409c12332611b69ea74
Nat-crit20 <greuelnathan74@gmail.com>
2023-06-16 09:26:09 -0500

    Updated firebaseConfig

--- App.js
+++ App.js
@@ -26,7 +26,14 @@ import { Alert } from "react-native";
 import { useNetInfo } from "@react-native-community/netinfo";
 
 // Your web app's Firebase configuration
-const firebaseConfig = FIREBASECONFIG;
+const firebaseConfig = {
+  apiKey: "AIzaSyBjfIKhlG0-GK5k5-f90IxnxBzpoIyqlNg",
+  authDomain: "chat-app-3c693.firebaseapp.com",
+  projectId: "chat-app-3c693",
+  storageBucket: "chat-app-3c693.appspot.com",
+  messagingSenderId: "94166338002",
+  appId: "1:94166338002:web:574152e046ee83b80b200b",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
